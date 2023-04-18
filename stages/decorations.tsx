13bd49bcd9ee0fc94c9d517aab46bffa9e27a63e
xtremind <guillaume.dagraca@gmail.com>
2023-04-18 14:45:32 +0200

    adding map files

---
+++ stages/decorations.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="decorations" tilewidth="8" tileheight="8" tilecount="4" columns="4">
+ <image source="../src/assets/img/decorations.png" width="32" height="8"/>
+ <tile id="0">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
